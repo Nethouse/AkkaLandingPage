cec60f4aa50f6ae2daef281fd939416eec7dd690
rogeralsing <rogeralsing@gmail.com>
2015-04-14 16:40:12 +0200

    akka site

---
+++ assets/ContactList.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+
+    $('.gmap-marker').each(function (ind, el) {
+        var lat = $(el).attr('data-map-lat');
+        var lng = $(el).attr('data-map-lng');
+        console.log(lat + ':' + lng);
+        var map = new GMaps({
+            div: $(el).attr('id'),
+            lat: lat,
+            lng: lng,
+            zoom: 16,
+            scrollwheel: false
+        });
+
+        map.addMarker({
+            lat: lat,
+            lng: lng,
+            title: $(el).attr('data-map-title')
+        });
+    });
+});
---
+++ assets/NewsListing.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    var btn = $('.news-list .load-more-news');
+    btn.attr('href', '#load-more');
+    btn.click(function () {
+        var pageID = $(this).attr('data-parent');
+        var start = +$(this).attr('data-start');
+        var max = +$(this).attr('data-max');
+        var year = $(this).attr('data-year');
+        $(this).attr('data-start', start + max);
+        console.log(start + " " + max + " " + year);
+        $.get(
+            '/AjaxTemplate/MoreNews',
+            { 'id': pageID, 'skip': start, 'year': year, 'max': max },
+            function (data) {
+                var dataEls = $(data);
+                $('.news-container').append(dataEls);
+                readyNresize();
+                if (dataEls.find('.news-listing-ended').length > 0) {
+                    $(btn).hide();
+                    $(btn).off('click');
+                }
+            }
+        );
+
+        return false;
+    });
+});
